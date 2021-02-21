3dac765cb41076cc78cb74ad481de53c903b856d
川口美咲 <misaki@arcphilia.co.jp>
2021-02-22 00:46:55 +0900

    [Add]進む処理を追加

--- app/src/main/java/com/misakikawaguchi/mediasample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/mediasample/MainActivity.kt
@@ -119,4 +119,20 @@ class MainActivity : AppCompatActivity() {
         _player?.seekTo(0)
     }
 
+    // 進む処理
+    fun onForwardButtonClick(view: View) {
+        // フィールドのプレーヤーがnullじゃなかったら
+        _player?.let {
+            // 現在再生中のメディアファイルの長さを取得
+            val duration = it.duration
+            // 再生位置を終端に変更
+            it.seekTo(duration)
+
+            // 再生中でないなら
+            if(!it.isPlaying) {
+                // 再生を開始
+                it.start()
+            }
+        }
+    }
 }
