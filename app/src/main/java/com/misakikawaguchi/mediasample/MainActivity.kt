72a839c0b0c50bafdb5bc62b4b507f749f13607b
川口美咲 <misaki@arcphilia.co.jp>
2021-02-21 23:50:41 +0900

    [Add]メディアプレーヤーフィールド作成

--- app/src/main/java/com/misakikawaguchi/mediasample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/mediasample/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.misakikawaguchi.mediasample
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // メディアプレーヤーフィールド
+    private var _player: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
